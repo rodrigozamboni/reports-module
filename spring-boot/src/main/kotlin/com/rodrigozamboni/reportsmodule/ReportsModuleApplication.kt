297b95da5c1afb2432807744f75cfbacb59e2f25
Rodrigo Zamboni <jrzamboni@mac-xamba.local>
2023-09-30 20:23:45 -0300

    first commit

---
+++ spring-boot/src/main/kotlin/com/rodrigozamboni/reportsmodule/ReportsModuleApplication.kt
@@ -0,0 +1,11 @@
+package com.rodrigozamboni.reportsmodule
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReportsModuleApplication
+
+fun main(args: Array<String>) {
+	runApplication<ReportsModuleApplication>(*args)
+}
